55fe8993b36b6014495eb8d054fa86601c595a1c
Louis De Macedo <louis.de.macedo.ldm@gmail.com>
2022-07-28 10:12:57 +0200

    fix: added missing tsconfig file

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,12 @@ import type { Config } from "@jest/types"
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    "^.+\\.ts$": "ts-jest"
+  },
+  moduleNameMapper: {
+    "test/(.*)$": "<rootDir>/test/$1"
+  }
 };
 
 export default config
