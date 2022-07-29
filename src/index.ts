66b618ef37c9ec7c71183406766604426718a420
Louis De Macedo <louis.de.macedo.ldm@gmail.com>
2022-07-29 13:17:24 +0200

    feature: added swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import dotenv from "dotenv"
 import fastify, { FastifyInstance } from "fastify"
-import * as routes from "./routes"
 import { db } from "./dbconfig"
+import swagger from "@fastify/swagger"
+import * as routes from "./routes"
 
 dotenv.config({ path: "../.env" })
 
@@ -9,9 +10,116 @@ db.migrate.latest({ directory: "dest/migrations" })
 
 const server: FastifyInstance = fastify({})
 
-server.get("/customers", {}, routes.getCustomers)
-server.post("/customers", {}, routes.postCustomers)
-server.post("/customers/:id/kpi", {}, routes.postCustomersKpi)
+server.register(swagger, {
+  swagger: {
+    info: {
+      title: "API CRUD Documentation",
+      description: "The swagger documentation for API CRUD",
+      version: "0.1.6",
+    },
+    host: `127.0.0.1:${process.env.HOST_PORT}`,
+    schemes: ["http"],
+    consumes: ["application/json"],
+    produces: ["application/json"],
+  },
+  exposeRoute: true,
+})
+
+server.register(async (server) => {
+  server.get(
+    "/customers",
+    {
+      schema: {
+        description: "Get a list of customers",
+        response: {
+          200: {
+            description: "Successfully get a list of customers",
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                username: {
+                  type: "string",
+                },
+                lastname: {
+                  type: "string",
+                },
+                birthdate: {
+                  type: "string",
+                  format: "date-time",
+                },
+                kpis: {
+                  type: "array",
+                  items: {
+                    type: "object",
+                    properties: {
+                      number_purchase: {
+                        type: "number",
+                      },
+                      store: {
+                        type: "string",
+                      },
+                      status: {
+                        type: "string",
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    routes.getCustomers
+  )
+
+  server.post(
+    "/customers",
+    {
+      schema: {
+        description: "Create a new customer in the database",
+        body: {
+          type: "object",
+          properties: {
+            username: {
+              type: "string",
+            },
+            lastname: {
+              type: "string",
+            },
+            birthdate: {
+              type: "string",
+              format: "date-time",
+            },
+          },
+        },
+      },
+    },
+    routes.postCustomers
+  )
+
+  server.post(
+    "/customers/:id/kpi",
+    {
+      schema: {
+        description: "Create a KPI for a customer",
+        body: {
+          type: "object",
+          properties: {
+            number_purchase: {
+              type: "number",
+            },
+            store: {
+              type: "string",
+            },
+          },
+        },
+      },
+    },
+    routes.postCustomersKpi
+  )
+})
 
 server.listen(
   { host: String(process.env.HOST_IP), port: Number(process.env.HOST_PORT) },
